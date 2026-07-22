b1446b788cf6922ca9cdcd51dd4d0ff61ae8489b
sriharshachandaluri <232310587+sriharshachandaluri@users.noreply.github.com>
2026-07-22 17:57:43 +0530

    Time: 0 ms (100%), Space: 19.4 MB (23.67%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -2,10 +2,8 @@ class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         if not nums:
             return []
-
         result = []
         j = 0
-
         for i in range(1, len(nums)):
             if nums[i] - nums[i - 1] != 1:
                 if i - j == 1:
@@ -13,8 +11,6 @@ class Solution:
                 else:
                     result.append(f"{nums[j]}->{nums[i - 1]}")
                 j = i
-
-        # Process the last range
         if len(nums) - j == 1:
             result.append(str(nums[j]))
         else:
